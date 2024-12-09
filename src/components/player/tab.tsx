2b9f0dd729b37be89d4acc199e5b6c33e046ee68
chirayu-wal <chirayu.a@veltris.com>
2024-12-10 01:56:44 +0530

    add new server

--- src/components/player/tab.tsx
+++ src/components/player/tab.tsx
@@ -13,12 +13,19 @@ export const servers = [
   {
     name: "Server 2",
     getter: (type: string, id: string, ss?: string, ep?: string) =>
-      `https://vidsrc.cc/v2/embed/${type}/${id}${
+      `https://vidbinge.dev/embed/${type}/${id}${
         type == "tv" ? `/${ss || 1}/${ep || 1}` : ""
       }`,
   },
   {
     name: "Server 3",
+    getter: (type: string, id: string, ss?: string, ep?: string) =>
+      `https://vidsrc.cc/v2/embed/${type}/${id}${
+        type == "tv" ? `/${ss || 1}/${ep || 1}` : ""
+      }`,
+  },
+  {
+    name: "Server 4",
     getter: (type: string, id: string, ss?: string, ep?: string) =>
       `https://vidsrc.in/embed/${type}?tmdb=${id}${
         type == "tv" ? `&season=${ss || 1}&episode=${ep || 1}` : ""
